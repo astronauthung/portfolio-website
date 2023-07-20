9f259edc2d0bdc30b4a1a08b2ebe9481367b396b
moonstruck19 <hungnp.21it@vku.udn.vn>
2023-07-20 15:47:51 +0700

    add footer and header

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
 const app = express()
 const port = 3000
 
+const handlebars = require('express-handlebars');
+
+app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.engine(
+  'hbs',
+  handlebars.engine({
+      extname: '.hbs',
+  }),
+);
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home');
 })
 
 app.listen(port, () => {
